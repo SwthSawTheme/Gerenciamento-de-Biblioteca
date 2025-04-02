a19a647cbac8a5aa97a8d6be95dbf958d9c566db
SwthSawTheme <sawtheme1@gmail.com>
2025-04-02 19:41:01 -0300

    Script de criação do Bando e da Tabela livros criado

---
+++ app/database/data.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+def createDB():
+    
+    tabela = """
+    CREATE TABLE IF NOT EXISTS Livros (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    TITULO VARCHAR(25) NOT NULL,
+    AUTOR VARCHAR(25) NOT NULL,
+    ANO_PUBLICACAO INTEGER,
+    DISPONIVEL BOOLEAN
+    )
+"""
+    try:
+        conexao = sqlite3.connect("Biblioteca.db")
+        cursor = conexao.cursor()
+        
+        cursor.execute(tabela)
+        conexao.commit()
+        print("Tabela Livros criada com sucesso!")
+    except Exception as e:
+        print(f"Erro: {e}")
+    finally:
+        if conexao:
+            conexao.close()
+            print("Conexão fechada com sucesso!")
+
+if __name__ == "__main__":
+    createDB()
+    print("Banco de dados criado com sucesso!")
+        
